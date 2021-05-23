611b0a9fd7868de0ded48799ef434b662c131918
Gabeh98 <bielhenry09@gmail.com>
2021-05-23 14:43:55 -0300

    First commit server base

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+const CONNECTION_URL ='mongodb+srv://admin:admin@cluster0.fhezf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 8081;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=> app.listen(PORT,()=>console.log(`Server running at port: ${PORT}`)))
+    .catch((error)=>console.log(error.message));
+
+mongoose.set('useFindAndModify',false);
