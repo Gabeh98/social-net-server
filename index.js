ccbed720eef279b1f07ecd2de48566a96fa83ed0
Gabeh98 <bielhenry09@gmail.com>
2021-05-24 16:57:59 -0300

    Melhorando visibilidade das portas

--- index.js
+++ index.js
@@ -9,15 +9,14 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
+const PORT = process.env.PORT || 8081;
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running at port: ${process.env.PORT}`)
-    )
+    app.listen(PORT, () => console.log(`Server running at port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
 
