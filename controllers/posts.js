e86a9d1f6c8f159df8834e80160ed40e75b69fdf
Gabeh98 <bielhenry09@gmail.com>
2021-05-24 14:16:42 -0300

    Adicionando metodo de delete

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,4 @@
-import  mongoose  from "mongoose";
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -25,9 +25,17 @@ export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
   const post = req.body;
   if (!mongoose.Types.ObjectId.isValid(_id))
-    return res.status(404).send("No post with that id");
+    return res.status(404).send("No Post with that id");
   const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
     new: true,
   });
   res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No Delete with that id");
+  await PostMessage.findByIdAndDelete(id);
+  res.json({ message: "Post deleted succesfully" });
 };
