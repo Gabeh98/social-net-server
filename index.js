a67dd9f7f503705004db8a2772ad9f9c2cfdc79f
Gabeh98 <bielhenry09@gmail.com>
2021-05-24 16:54:13 -0300

    Adicionando variavel Global

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/post.js";
+import dotenv from "dotenv";
 const app = express();
-
+dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://admin:admin@cluster0.fhezf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 8081;
-
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running at port: ${PORT}`))
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running at port: ${process.env.PORT}`)
+    )
   )
   .catch((error) => console.log(error.message));
 
